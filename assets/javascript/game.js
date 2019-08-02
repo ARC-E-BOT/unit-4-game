5ecf51988b21859a44f075baaa950d469dc8f75f
Adam Cooper <frankidyfrank3@gmail.com>
2019-08-02 17:14:14 -0500

    Created: reset function for play area added: a small amount of html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,19 @@
 //declaring the character objects
 const characterOne = {
     health: 100,
-    attack: 12,
+    attack: 13,
     counterAttack: 10
 };
 
 const characterTwo = {
     health: 120,
-    attack: 12,
+    attack: 10,
     counterAttack: 15
 };
 
 const characterThree = {
     health: 140,
-    attack: 12,
+    attack: 14,
     counterAttack: 20
 };
 
@@ -23,12 +23,17 @@ const characterFour = {
     counterAttack: 25
 };
 
-
 //declaring the global variables
 let chosenCharacter = undefined;
 let chosenEnemy = undefined;
 let npcCharacters = [characterOne, characterTwo, characterThree, characterFour];
 
+//this function resets the play area variables
+function resetPlayArea(){
+    chosenCharacter = undefined;
+    chosenEnemy = undefined;
+    npcCharacters = [characterOne, characterTwo, characterThree, characterFour];
+}
 
 //get the index of the char in the npc char array and remove the char from the array to signal that the char has been chosen in one way or another
 function characterSelection(obj){
