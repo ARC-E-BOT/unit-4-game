f46aac329deed8b3974e7dc63b07205d04ab294b
Adam Cooper <frankidyfrank3@gmail.com>
2019-08-02 16:01:34 -0500

    Created: basic character objects

---
+++ assets/javascript/game.js
@@ -0,0 +1,24 @@
+//declaring the character objects
+const characterOne = {
+    health: 0,
+    attack: 0,
+    counterAttack: 0
+};
+
+const characterTwo = {
+    health: 0,
+    attack: 0,
+    counterAttack: 0
+};
+
+const characterThree = {
+    health: 0,
+    attack: 0,
+    counterAttack: 0
+};
+
+const characterFour = {
+    health: 0,
+    attack: 0,
+    counterAttack: 0
+};
