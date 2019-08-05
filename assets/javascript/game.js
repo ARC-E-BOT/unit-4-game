b16e035e2301a3e0d61413ac56f0d3c18f3b266e
Adam Cooper <frankidyfrank3@gmail.com>
2019-08-05 14:25:19 -0500

    last push before i left school today so i can use my desktop at home

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -34,42 +34,44 @@ const characterFour = {
 //declaring the global variables
 let chosenCharacter = undefined;
 let chosenEnemy = undefined;
+let isPlayable = false;
 let npcCharacters = [characterOne, characterTwo, characterThree, characterFour];
 
 
 //call elements from the document into global const variables 
 const selectCharacter = document.getElementById("character-select");
-const charPlayer = document.getElementById("character-player")
-const charDefence = document.getElementById("character-defence");
 const arenaDiv = document.getElementById("arena");
 
 
 //initialize the characters and display them on the page
-npcCharacters.forEach(char => {
-    //create new div html element for each char
-    let newDiv = createDiv(char, "", "");
+function loadNPCs(){
+    npcCharacters.forEach(char => {
+        //create new div html element for each char
+        let newDiv = createDiv(char, "", "");
 
-    //set a click event listener for each new div so the player can choose 
-    newDiv.addEventListener("click", function(){
-        chosenCharacter = char;
-        characterSelection(char);
+        //set a click event listener for each new div so the player can choose 
+        newDiv.addEventListener("click", function(){
+            if (isPlayable === false) return;
+            chosenCharacter = char;
+            characterSelection(char);
 
-        //create new div for the character and set all classes, attribute's and innerHTML so it can be shown in the player div on the page properly
-        let playerDiv = createDiv(chosenCharacter, "player", ` id="character-health"`);
-        playerDiv.classList.add("vs");
-        let newWideDiv = createThirdWide("player-attack", playerDiv);
+            //create new div for the character and set all classes, attribute's and innerHTML so it can be shown in the player div on the page properly
+            let playerDiv = createDiv(chosenCharacter, "player", ` id="character-health"`);
+            playerDiv.classList.add("vs");
+            let newWideDiv = createThirdWide("player-attack", playerDiv);
 
-        //adding the newly created character div and clearing the character-select div
-        arenaDiv.prepend(newWideDiv);
-        selectCharacter.innerHTML = "";
+            //adding the newly created character div and clearing the character-select div
+            arenaDiv.prepend(newWideDiv);
+            selectCharacter.innerHTML = "";
 
-        //once the player is setup, setup the enemy's
-        setupEnemys();
-    })
+            //once the player is setup, setup the enemy's
+            setupEnemys();
+        })
 
-    //adding the div to the page 
-    selectCharacter.appendChild(newDiv);
-});
+        //adding the div to the page 
+        selectCharacter.appendChild(newDiv);
+    });
+}
 
 
 //sets up the enemy's after the player has chosen there character
@@ -98,60 +100,105 @@ function setupEnemys(){
     });
 }
 
-// `
-// <div id="vs">
-//     <img src="./assets/images/swords.png" width="100px">
-//     <br>
-//     <button id="attack">Attack!</button>
-// </div>
-// `
 
+loadNPCs();
 document.getElementById("ready-to-play").addEventListener("click", function(){
     setupPlayArea();
+    isPlayable = true;
 })
 
 
 // setup the play area once the user has read the rules and is ready to play!
 function setupPlayArea(){
+    //clear the arena div so the play field is clean for the new Attack button 
     arenaDiv.innerHTML = "";
+
+    //create the div for the aattack button and swords 
     let newDiv = document.createElement("div");
     newDiv.id = "vs";
+
+    //create the image tag so the swords can be brought in
     let swordsImg = document.createElement("img");
+
+    //add in the line brake so the button and image line up correctly 
     let lineBreak = document.createElement("br");
+    
+    //set the image src/url and the width of the image
     swordsImg.src = "./assets/images/swords.png";
     swordsImg.width = "100";
+
+    //create the attack button, set the text, and the id to attack
     let attackButton = document.createElement("button");
     attackButton.innerText = "Attack!";
     attackButton.id = "attack";
+
+    // create the listener on the attack button so when you click it. it will do the attack
     createAttackListener(attackButton);
+
+    //adding the elements we just created to the div as children
     newDiv.appendChild(swordsImg);
     newDiv.appendChild(lineBreak);
     newDiv.appendChild(attackButton);
+
+    //create the 1/3 div in the attack area so the arena looks right when adding characters, and adding the newly created div from above to this new third wide div
     let newWideDiv = createThirdWide("attack-button", newDiv);
+
+    //adding the new wide div to the arena div
     arenaDiv.appendChild(newWideDiv)
 }
 
 //creates a listener on the attack button 
 function createAttackListener(attackButton){
     attackButton.addEventListener("click", function(){
+        //if there is no character's or enemy's dont attack
         if(chosenCharacter !== undefined && chosenEnemy !== undefined){
+
+            //adding more attack to the player
             chosenCharacter.attack = attackMultiplier(chosenCharacter);
+
+            //attacking the enemy and updating the health of the of both so it is displayed on the page
             attack(chosenCharacter, chosenEnemy);
             updateHealthID();
-            if(chosenCharacter.health < 0){
+
+            //if the character's health is less than or equals 0 meaning you died!
+            if(chosenCharacter.health <= 0){
                 document.getElementById("player").innerHTML = `<h1>Game over you died! your heath was: ${chosenCharacter.health}</h1>`;
-                chosenCharacter = undefined
-            } else if(chosenEnemy.health < 0){
+                chosenCharacter = undefined;
+                endgame();
+            }
+            //if the Enemy's health is less than or equals 0 meaning you killed the enemy!
+            else if(chosenEnemy.health <= 0){
+
+                //get the enemy div and remove it if they have died
                 const defenderDiv = document.getElementById("defender");
                 defenderDiv.remove();
-                `<h1>You have killed: ${chosenEnemy.name}</h1>`;
+                // `<h1>You have killed: ${chosenEnemy.name}</h1>`;
+
+                //set the enemy to undefined signaling that you have no one to fight
                 chosenEnemy = undefined;
+
+                //if no characters are in the array you win!
                 if(npcCharacters.length === 0){
                     alert("You Win!!!")
+                    endgame();
                 }
             }
         }
     })
+}
+
+function endgame(){
+    arenaDiv.innerHTML = `
+    <div id="welcome">
+        <h1>Welcome to the Minecraft RPG!</h1>
+        <p>
+            How to play, click on the character that you would like to be your avatar for the rest of the game. any avatars left will become your enemy's and you have to defeat all of them to win the game. once you click the ready to play button below you will see your attack button, choose your opponent and get playing!!
+        </p>
+        <button id="ready-to-play">Ready To Play!</button>
+    </div>
+    `;
+    npcCharacters = [characterOne, characterTwo, characterThree, characterFour];
+    loadNPCs();
 }
 
 //this creates a parent div that is 33.3% wide to make the play area look better for fighting
