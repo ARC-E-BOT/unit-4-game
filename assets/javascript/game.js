a419d8357bd7f61fe2f14d60b1a7f1bd817ec086
Adam Cooper <frankidyfrank3@gmail.com>
2019-08-04 23:50:58 -0500

    changed: the logic to function with the new layout created: issue with button not properly lining up where it should be ...

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -41,6 +41,7 @@ let npcCharacters = [characterOne, characterTwo, characterThree, characterFour];
 const selectCharacter = document.getElementById("character-select");
 const charPlayer = document.getElementById("character-player")
 const charDefence = document.getElementById("character-defence");
+const arenaDiv = document.getElementById("arena");
 
 
 //initialize the characters and display them on the page
@@ -55,9 +56,10 @@ npcCharacters.forEach(char => {
 
         //create new div for the character and set all classes, attribute's and innerHTML so it can be shown in the player div on the page properly
         let playerDiv = createDiv(chosenCharacter, "player", ` id="character-health"`);
+        playerDiv.classList.add("vs");
 
         //adding the newly created character div and clearing the character-select div
-        charPlayer.appendChild(playerDiv);
+        arenaDiv.prepend(playerDiv);
         selectCharacter.innerHTML = "";
 
         //once the player is setup, setup the enemy's
@@ -79,11 +81,12 @@ function setupEnemys(){
                 chosenEnemy = enemy;
                 characterSelection(chosenEnemy);
                 //create new div for the character and set all classes, attribute's and innerHTML so it can be shown in the player div on the page properly
-                charDefence.innerHTML = ""
+                selectCharacter.innerHTML = ""
                 let enemyDiv = createDiv(chosenEnemy, "defender", ` id="enemy-health"`);
+                enemyDiv.classList.add("vs")
 
                 //adding the newly created enemy div to the defence div and clearing the character-enemy div so it can be refreshed
-                charDefence.appendChild(enemyDiv);
+                arenaDiv.appendChild(enemyDiv);
                 selectCharacter.innerHTML = "";
 
                 setupEnemys();
@@ -93,23 +96,61 @@ function setupEnemys(){
     });
 }
 
-document.getElementById("attack").addEventListener("click", function(){
-    if(chosenCharacter !== undefined && chosenEnemy !== undefined){
-        chosenCharacter.attack = attackMultiplier(chosenCharacter);
-        attack(chosenCharacter, chosenEnemy);
-        updateHealthID();
-        if(chosenCharacter.health < 0){
-            charPlayer.innerHTML = `<h1>Game over you died! your heath was: ${chosenCharacter.health}</h1>`;
-            chosenCharacter = undefined
-        } else if(chosenEnemy.health < 0){
-            charDefence.innerHTML = `<h1>You have killed: ${chosenEnemy.name}</h1>`;
-            chosenEnemy = undefined;
-            if(npcCharacters.length === 0){
-                alert("You Win!!!")
+// `
+// <div id="vs">
+//     <img src="./assets/images/swords.png" width="100px">
+//     <br>
+//     <button id="attack">Attack!</button>
+// </div>
+// `
+
+document.getElementById("ready-to-play").addEventListener("click", function(){
+    setupPlayArea();
+})
+
+
+// setup the play area once the user has read the rules and is ready to play!
+function setupPlayArea(){
+    arenaDiv.innerHTML = "";
+    let newDiv = document.createElement("div");
+    newDiv.id = "vs";
+    let swordsImg = document.createElement("img");
+    let lineBreak = document.createElement("br");
+    swordsImg.src = "./assets/images/swords.png";
+    swordsImg.width = "100";
+    let attackButton = document.createElement("button");
+    attackButton.innerText = "Attack!";
+    attackButton.id = "attack";
+    createAttackListener(attackButton);
+    newDiv.appendChild(swordsImg);
+    newDiv.appendChild(lineBreak);
+    newDiv.appendChild(attackButton);
+    arenaDiv.appendChild(newDiv)
+}
+
+//creates a listener on the attack button 
+function createAttackListener(attackButton){
+    attackButton.addEventListener("click", function(){
+        if(chosenCharacter !== undefined && chosenEnemy !== undefined){
+            chosenCharacter.attack = attackMultiplier(chosenCharacter);
+            attack(chosenCharacter, chosenEnemy);
+            updateHealthID();
+            if(chosenCharacter.health < 0){
+                document.getElementById("player").innerHTML = `<h1>Game over you died! your heath was: ${chosenCharacter.health}</h1>`;
+                chosenCharacter = undefined
+            } else if(chosenEnemy.health < 0){
+                const defenderDiv = document.getElementById("defender");
+                defenderDiv.remove();
+                `<h1>You have killed: ${chosenEnemy.name}</h1>`;
+                chosenEnemy = undefined;
+                if(npcCharacters.length === 0){
+                    alert("You Win!!!")
+                }
             }
         }
-    }
-})
+    })
+}
+
 
 //creates character div's so I don't have to copy and paste the same code about a million times
 function createDiv(char, id, healthID){
@@ -123,8 +164,8 @@ function createDiv(char, id, healthID){
 
 //updates the health of the player and enemy so that the player can see what is going on
 function updateHealthID(){
-    document.getElementById("character-health").textContent = `${chosenCharacter.name} Health: ${chosenCharacter.health}`;
-    document.getElementById("enemy-health").textContent = `${chosenEnemy.name} Health: ${chosenEnemy.health}`;
+    document.getElementById("character-health").textContent = `Health: ${chosenCharacter.health}`;
+    document.getElementById("enemy-health").textContent = `Health: ${chosenEnemy.health}`;
 }
 
 //subtract the attack and counter attack from players health and enemy's health
