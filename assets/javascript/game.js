d5e9567546ed831c592b0ca5ed834171ab1863b7
Adam Cooper <frankidyfrank3@gmail.com>
2019-08-05 22:06:28 -0500

    added: more minecraft text LOL added: more style for said minecraft text commented: ALL of the game.js file and finished the logic
    not for future coop add "random mode"

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -43,6 +43,17 @@ const selectCharacter = document.getElementById("character-select");
 const arenaDiv = document.getElementById("arena");
 
 
+//load the NPC's on page load 
+loadNPCs();
+
+//add click event to the "Ready To Play!" button
+document.getElementById("ready-to-play").addEventListener("click", function(){
+    //sets up the play area and sets the is playable lock to true so the user may choose a character 
+    setupPlayArea();
+    isPlayable = true;
+})
+
+
 //initialize the characters and display them on the page
 function loadNPCs(){
     npcCharacters.forEach(char => {
@@ -83,6 +94,11 @@ function setupEnemys(){
             if(chosenEnemy === undefined){
                 chosenEnemy = enemy;
                 characterSelection(chosenEnemy);
+
+                //remove the previous death message if there is one
+                const defenderDiv = document.getElementById("defender");
+                if (defenderDiv !== null) defenderDiv.remove();
+
                 //create new div for the character and set all classes, attribute's and innerHTML so it can be shown in the player div on the page properly
                 selectCharacter.innerHTML = ""
                 let enemyDiv = createDiv(chosenEnemy, "defender", ` id="enemy-health"`);
@@ -99,13 +115,6 @@ function setupEnemys(){
         selectCharacter.appendChild(newDiv);
     });
 }
-
-
-loadNPCs();
-document.getElementById("ready-to-play").addEventListener("click", function(){
-    setupPlayArea();
-    isPlayable = true;
-})
 
 
 // setup the play area once the user has read the rules and is ready to play!
@@ -147,6 +156,7 @@ function setupPlayArea(){
     arenaDiv.appendChild(newWideDiv)
 }
 
+
 //creates a listener on the attack button 
 function createAttackListener(attackButton){
     attackButton.addEventListener("click", function(){
@@ -162,75 +172,169 @@ function createAttackListener(attackButton){
 
             //if the character's health is less than or equals 0 meaning you died!
             if(chosenCharacter.health <= 0){
-                document.getElementById("player").innerHTML = `<h1>Game over you died! your heath was: ${chosenCharacter.health}</h1>`;
+                //create a new div to replace the arena that signals the player has died
+                let newDiv = document.createElement("div");
+                newDiv.innerHTML = `<h1>Game over you died! your heath was: ${chosenCharacter.health}</h1>`;
+                newDiv.className = "death-message";
+
+                //clear all html out of the arena and set the Character to undefined
+                arenaDiv.innerHTML = "";
                 chosenCharacter = undefined;
-                endgame();
+
+                //create a new button that asks the user if they would like to replay
+                let newButton = document.createElement("button");
+                newButton.textContent = "Replay?";
+                newButton.className = "attack";
+                newButton.addEventListener("click", function(){
+                    endgame();
+                })
+
+                //appending the button to the new div and then appending the new div to the arena
+                newDiv.appendChild(newButton);
+                arenaDiv.appendChild(newDiv);
+                
             }
             //if the Enemy's health is less than or equals 0 meaning you killed the enemy!
             else if(chosenEnemy.health <= 0){
 
                 //get the enemy div and remove it if they have died
                 const defenderDiv = document.getElementById("defender");
-                defenderDiv.remove();
-                // `<h1>You have killed: ${chosenEnemy.name}</h1>`;
+                defenderDiv.innerHTML = `<h1>You have killed: ${chosenEnemy.name}</h1>`;
+                defenderDiv.className = "death-message";
 
                 //set the enemy to undefined signaling that you have no one to fight
                 chosenEnemy = undefined;
 
                 //if no characters are in the array you win!
                 if(npcCharacters.length === 0){
-                    alert("You Win!!!")
-                    endgame();
+                    let newDiv = document.createElement("div");
+                    newDiv.innerHTML = `<h1>Game over, YOU WIN! your heath was: ${chosenCharacter.health}</h1>`;
+                    newDiv.className = "death-message";
+
+                    //clear all html out of the arena and set the Character to undefined
+                    arenaDiv.innerHTML = "";
+                    chosenCharacter = undefined;
+
+                    //create a new button that asks the user if they would like to replay
+                    let newButton = document.createElement("button");
+                    newButton.textContent = "Replay?";
+                    newButton.className = "attack";
+                    newButton.addEventListener("click", function(){
+                        endgame();
+                    })
+
+                    //appending the button to the new div and then appending the new div to the arena
+                    newDiv.appendChild(newButton);
+                    arenaDiv.appendChild(newDiv);
                 }
             }
         }
     })
 }
 
+
+//to end the game
 function endgame(){
-    arenaDiv.innerHTML = `
-    <div id="welcome">
-        <h1>Welcome to the Minecraft RPG!</h1>
-        <p>
-            How to play, click on the character that you would like to be your avatar for the rest of the game. any avatars left will become your enemy's and you have to defeat all of them to win the game. once you click the ready to play button below you will see your attack button, choose your opponent and get playing!!
-        </p>
-        <button id="ready-to-play">Ready To Play!</button>
-    </div>
-    `;
+    //set the character and enemy to undefined and make the game not playable
+    chosenCharacter = undefined;
+    chosenEnemy = undefined;
+    isPlayable = false;
+
+    //empty the arena and the selectCharacter Div
+    arenaDiv.innerHTML = "";
+    selectCharacter.innerHTML = "";
+
+    //create the welcome menu and append it to the arena
+    let welcomeMenu = createWelcomeMenu();
+    arenaDiv.appendChild(welcomeMenu);
+
+    //reset the NPC's stats back to default and reset the npcCharacters array
+    resetNpc();
     npcCharacters = [characterOne, characterTwo, characterThree, characterFour];
+
+    //load the NPC's for the player to choose from at the beginning of the game
     loadNPCs();
 }
 
+
+// re-create the welcome menu that is shown on page load
+function createWelcomeMenu(){
+
+    //create the main div to house the menu and set said div's id
+    let newDiv = document.createElement("div");
+    newDiv.id = "welcome";
+
+    //create the menu's title text h1 tag and set its inner tex and the class name
+    let newHOne = document.createElement("h1");
+    newHOne.className = "title";
+    newHOne.innerText = "Welcome to the Minecraft RPG!";
+
+    //create the paragraph html element and set its text 
+    let newParagraph = document.createElement("p");
+    newParagraph.innerText = "How to play, click on the character that you would like to be your avatar for the rest of the game. any avatars left will become your enemy's and you have to defeat all of them to win the game. once you click the ready to play button below you will see your attack button, choose your opponent and get playing!!";
+    
+    //create the play button set its text to "Ready To Play!" and its id as well as add a click listener for it
+    let newPlayButton = document.createElement("button");
+    newPlayButton.innerText = "Ready To Play!";
+    newPlayButton.id = "ready-to-play";
+    newPlayButton.addEventListener("click", function(){
+        //setting up the play area and set is playable to true so the user may choose a character
+        setupPlayArea();
+        isPlayable = true;
+    })
+
+    //append all of the elements we just created to the main div and return the newDiv so it can be used back where this function was called
+    newDiv.appendChild(newHOne);
+    newDiv.appendChild(newParagraph);
+    newDiv.appendChild(newPlayButton);
+    return newDiv;
+}
+
+
 //this creates a parent div that is 33.3% wide to make the play area look better for fighting
 function createThirdWide(id, childDiv){
+    //create a new div and add a class pf "third-wide"
     let newWideDiv = document.createElement("div");
     newWideDiv.classList.add("third-wide");
+
+    //set the id to the passed in value and appending the passing div to this new div
     newWideDiv.id = id;
     newWideDiv.appendChild(childDiv);
+
+    //returning the new wide div so that it may be worked with outside the function
     return newWideDiv;
 }
 
+
 //creates character div's so I don't have to copy and paste the same code about a million times
-function createDiv(char, id, healthID){
+function createDiv(char, id, healthID,attack){
+    //create the player div and setting the attribute of the character's name
     let playerDiv = document.createElement("div");
     playerDiv.setAttribute("character", char.name);
+
+    //setting the class of character and the passed in id for the div
     playerDiv.classList.add("character");
     playerDiv.id = id;
-    playerDiv.innerHTML = `<h4 id="name-text">${char.name}</h4><img class="character-img" src="${char.img}"><h4${healthID}>Health: ${char.health}</h4>`;
+
+    //setting the inner html of the new div we just made and returning the new div so it can be worked with outside the function
+    playerDiv.innerHTML = `<h4 id="name-text">${char.name}</h4><img class="character-img" src="${char.img}"><h4${healthID}>HP: ${char.health} / Atk: ${char.attack}</h4>`;
     return playerDiv;
 }
 
+
 //updates the health of the player and enemy so that the player can see what is going on
 function updateHealthID(){
-    document.getElementById("character-health").textContent = `Health: ${chosenCharacter.health}`;
-    document.getElementById("enemy-health").textContent = `Health: ${chosenEnemy.health}`;
+    document.getElementById("character-health").textContent = `HP: ${chosenCharacter.health} / Atk: ${chosenCharacter.attack}`;
+    document.getElementById("enemy-health").textContent = `HP: ${chosenEnemy.health} / Atk: ${chosenEnemy.counterAttack}`;
 }
+
 
 //subtract the attack and counter attack from players health and enemy's health
 function attack(player, enemy){
     enemy.health -= player.attack;
     player.health -= enemy.counterAttack;
 }
+
 
 //this function resets the play area variables
 function resetPlayArea(){
@@ -239,14 +343,38 @@ function resetPlayArea(){
     npcCharacters = [characterOne, characterTwo, characterThree, characterFour];
 }
 
+
 //get the index of the char in the npc char array and remove the char from the array to signal that the char has been chosen in one way or another
 function characterSelection(obj){
     const index = npcCharacters.indexOf(obj);
     npcCharacters.splice(index,1);
 }
 
+
 //adding more attack points to the passed in character variable between 0 and the character's current attack level
 function attackMultiplier(char){
     return char.attack + 6;
 }
 
+
+function resetNpc(){
+    //resetting CharacterOne's stats
+    characterOne.health = 130;
+    characterOne.attack = 8;
+    characterOne.counterAttack = 7;
+
+    //resetting characterTwo's stats
+    characterTwo.health = 120;
+    characterTwo.attack = 5;
+    characterTwo.counterAttack = 12;
+    
+    //resetting characterTwo's stats
+    characterThree.health = 140;
+    characterThree.attack = 7;
+    characterThree.counterAttack = 16;
+    
+    //resetting characterTwo's stats
+    characterFour.health = 160;
+    characterFour.attack = 6;
+    characterFour.counterAttack = 20;
+}
